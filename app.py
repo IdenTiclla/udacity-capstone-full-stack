b0fa4fabe8a852a3f35a7a5b614909f1ec6de458
IdenTiclla <iden.ticlla@gmail.com>
2021-03-16 18:41:37 -0400

    add after request

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ import os
 from flask import Flask
 
 from flask_cors import CORS
-from database.models import setup_db
+from database.models import setup_db, Actor
 from auth.auth import AuthError, requires_auth
 def create_app(test_config=None):
 
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
 
     @app.route('/')
     def get_greeting():
