d340be3cab1c2359e891d9439f42b50680a373f9
IdenTiclla <iden.ticlla@gmail.com>
2021-03-15 22:11:50 -0400

    add requirements.txt and setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+export DATABASE_URL=postgres://hsaeytugtlrgov:0034e8d04ecf2d8afcbfd97dff379719a5cf3d39cbedbe0a33f7260b19582035@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d40hq1ji31csj
+export FLASK_APP=app.py
