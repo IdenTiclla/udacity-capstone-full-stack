186922d23492cc933b40b70dab6c9d8eed14778b
IdenTiclla <iden.ticlla@gmail.com>
2021-03-16 17:53:53 -0400

    add API_AUDIENCE

--- auth/auth.py
+++ auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'myauth-iden.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee'
+API_AUDIENCE = 'capstone'
 
 ## AuthError Exception
 '''
