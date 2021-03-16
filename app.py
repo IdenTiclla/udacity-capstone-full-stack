a9bcb9022a682158618726925dfdb49eece9e0be
IdenTiclla <iden.ticlla@gmail.com>
2021-03-16 18:52:35 -0400

    CORS

--- app.py
+++ app.py
@@ -8,12 +8,9 @@ def create_app(test_config=None):
 
     app = Flask(__name__)
     setup_db(app)
+
     CORS(app)
 
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
 
     @app.route('/')
     def get_greeting():
