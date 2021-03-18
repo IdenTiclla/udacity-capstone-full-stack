986de06c8f7dbc1a3aacba303bf351ae7707545e
IdenTiclla <iden.ticlla@gmail.com>
2021-03-17 21:18:44 -0400

    add patch actor

--- app.py
+++ app.py
@@ -20,6 +20,28 @@ def create_app(test_config=None):
     def be_cool():
         return "Be cool, man, be coooool! You're almost a FSND grad!"
 
+    @app.route('/actors/<int:id>', methods=['PATCH'])
+    def post_actor():
+        if request.method == "PATCH":
+            body = request.get_json()
+
+            name = body.get('name', None)
+            age = body.get('age', None)
+            gender = body.get('gender', None)
+            
+            actor = Actor.query.filter_by(id=id).one_or_none()
+            
+            actor.name = name
+            actor.age = age
+            actor.gender = gender
+
+            actor.update()
+
+            return jsonify({
+                'success': True,
+                'updated_actor': id,
+                'total_actors': len(Actor.query.all())
+            })
     return app
 
 app = create_app()
